3490dca0476f7a7c11af05aa6a17090cd7c78ca0
Daniel Rodriguez <arevdevgames@gmail.com>
2024-05-29 09:44:34 -0600

    first commit

---
+++ src/evaluation/Cliente.java
@@ -0,0 +1,81 @@
+package evaluation;
+
+public class Cliente {
+    private int id;
+    private String codigo;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+
+    public Cliente(int id, String nombre, String apellido, String email, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+        //-----------------------
+        int numero1 = (int) (Math.random() * 9) + 1;
+        int numero2 = (int) (Math.random() * 9) + 1;
+        int numero3 = (int) (Math.random() * 9) + 1;
+
+        String nombreIniciales = nombre.substring(0, 2);
+        String apellidoIniciales = apellido.substring(0, 2);
+
+        this.codigo = nombreIniciales+apellidoIniciales+numero1+numero2+numero3;
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Cliente {" +
+                "Id=" + id +
+                ", Codigo="+codigo+
+                ", Nombre='" + nombre + '\'' +
+                ", Apellido='" + apellido + '\'' +
+                ", Email='" + email + '\'' +
+                ", Telefono='" + telefono + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
---
+++ src/evaluation/Main.java
@@ -0,0 +1,59 @@
+package evaluation;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+
+        int id = 0;
+        String nombre;
+        String apellido;
+        String email;
+        String telefono;
+
+        boolean stop = false;
+
+        while (!stop)
+        {
+            System.out.println("Ingrese el Id del cliente");
+            id = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Ingrese el Nombre del cliente");
+            nombre = scanner.nextLine();
+
+            System.out.println("Ingrese el Apellido del cliente");
+            apellido = scanner.nextLine();
+
+            System.out.println("Ingrese el Email del cliente");
+            email = scanner.nextLine();
+
+            System.out.println("Ingrese el Telefono del cliente");
+            telefono = scanner.nextLine();
+
+            Cliente c = new Cliente(id, nombre, apellido, email, telefono);
+            clientes.add(c);
+
+            System.out.println("---------------------------------");
+            System.out.println("Cliente Agregado exitosamente");
+            System.out.println("¿Desea continuar? S/N");
+            stop = scanner.nextLine().equalsIgnoreCase("n");
+
+        }
+
+        Iterator<Cliente> it = clientes.iterator();
+
+        System.out.println("---------------------------------");
+        System.out.println("Los Clientes Registrados son: ");
+
+        while(it.hasNext()){
+            System.out.println("\n");
+            System.out.println(it.next());
+        }
+    }
+}
